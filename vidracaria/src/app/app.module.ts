e078f0271588855886e7c758f761d5658366f24c
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-03-22 02:20:50 -0300

    criando e aplicando diretiva de atributo

--- vidracaria/src/app/app.module.ts
+++ vidracaria/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatCardModule } from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    RedDirective
   ],
   imports: [
     BrowserModule,
