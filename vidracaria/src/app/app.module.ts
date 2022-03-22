683627758a0ea34fca6c0e9eaa949dc80e9516d2
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-03-22 01:22:18 -0300

    criacao do componete home

--- vidracaria/src/app/app.module.ts
+++ vidracaria/src/app/app.module.ts
@@ -11,14 +11,17 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +29,8 @@ import {MatListModule} from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
     
   ],
   providers: [],
