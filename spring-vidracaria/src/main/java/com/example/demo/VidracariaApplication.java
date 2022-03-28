4dc804e64ba1b3ae96d7ac1fbec932a49e60933f
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-03-28 16:33:09 -0300

    inicio spring vidracaria

---
+++ spring-vidracaria/src/main/java/com/example/demo/VidracariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidracariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidracariaApplication.class, args);
+	}
+
+}
