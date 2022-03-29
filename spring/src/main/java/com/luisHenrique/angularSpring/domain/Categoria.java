ffe7d88c4b4d878c6a78982d55cf083f0f6cf707
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-03-29 16:04:38 -0300

    finalizando classes de dominio

--- spring/src/main/java/com/luisHenrique/angularSpring/domain/Categoria.java
+++ spring/src/main/java/com/luisHenrique/angularSpring/domain/Categoria.java
@@ -1,15 +1,27 @@
 package com.luisHenrique.angularSpring.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Categoria implements Serializable {	
+	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Integer id;
 	private String nome;
 	private String descricao;
 	
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> Livros = new ArrayList<>();
 
 	public Categoria() {
--- spring/src/main/java/com/luisHenrique/angularSpring/domain/Livro.java
+++ spring/src/main/java/com/luisHenrique/angularSpring/domain/Livro.java
@@ -1,13 +1,28 @@
 package com.luisHenrique.angularSpring.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Livro implements Serializable {	
+	private static final long serialVersionUID = 1L; 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String nome_autor;
 	private String texto;
 
+	@ManyToOne
+	@JoinColumn(name = "Categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
